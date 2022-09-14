86f3d8b65869ae3d64fd884e2e48ea7b310f084e
AkhilJoga <111341918+AkhilJoga@users.noreply.github.com>
2022-09-15 02:32:08 +0530

    gitadding DigitalBook-FronEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBooksComponent } from './components/search-books/search-books.component';
+import { AuthorComponent } from './components/author/author.component';
+import { CreateBooksComponent } from './components/create-books/create-books.component';
+import { BookRegisterComponent } from './components/book-register/book-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBooksComponent,
+    AuthorComponent,
+    CreateBooksComponent,
+    BookRegisterComponent
   ],
   imports: [
     BrowserModule,
